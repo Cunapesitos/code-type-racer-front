26296d8c9b9fa92a6b99479ac943597e708174fe
Jorge Rodrigo Torrez Aramayo <jorgerodrigotorrez@gmail.com>
2021-03-28 21:47:16 -0400

    type errors fixed

--- src/config/Activity.ts
+++ src/config/Activity.ts
@@ -3,18 +3,18 @@ import { Config } from "./Config";
 // eslint-disable-next-line
 const log = (body: any = {}): void => {
     if (!Config.PRODUCTION) {
-        // tslint:disable-next-line:no-console
+        // eslint-disable-next-line
         console.log(body);
     }
 };
 
 const info = (message: string): void => {
-    // tslint:disable-next-line:no-console
+    // eslint-disable-next-line
     console.info(message);
 };
 
 const error = (message: string): void => {
-    // tslint:disable-next-line:no-console
+    // eslint-disable-next-line
     console.error(message);
 };
 
